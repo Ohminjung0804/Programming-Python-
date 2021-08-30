348fce673b9534e4b443492133d87a63a358ce33
OHMINJUNG <w2009@e-mirim.hs.kr>
2021-08-30 12:08:26 +0900

    package 2/2

--- modul, package/do.py
+++ modul, package/do.py
@@ -1,3 +1,17 @@
-import my_modul
+# import my_modul
+#
+# my_modul.나라친구()
 
-my_modul.나라친구()
+# import tour.jeonnam as tj
+# tj.jeonnam.채리()
+#
+# from tour import jeonnam as je
+# je.채리()
+#
+# from tour.jeonnam import 채리 as cherry
+# cherry()
+
+from tour import *
+jeonnam.채리()
+gp = gangwondo.GangwondoPackage()
+print(gp)
