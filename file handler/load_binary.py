98121af25bdeac7099e6835e437c0f99c63d22db
OHMINJUNG <w2009@e-mirim.hs.kr>
2021-10-08 14:40:25 +0900

    binary to text

--- file handler/load_binary.py
+++ file handler/load_binary.py
@@ -1,3 +1,9 @@
-with open('binary.bin','rb') as f :
+with open('text.bin','wb') as f :
+    f.write(b'\xea\xb0\x80')
+
+input()
+
+
+with open('binary.bin','r',encoding='utf-8') as f :
     data = f.read()   #b'\xff\x00\x7f': 111111110000000001111111
 print(data)
